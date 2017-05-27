dae09ad7c8a7f1ee75d994bc7c94fa0ff4aeb35a
Julien Verneuil <grz.zrg@gmail.com>
2017-05-27 02:26:48 +0200

    add xypad controller format controller title with GLSL type fix antialiasing issues

--- client/js/controls.js
+++ client/js/controls.js
@@ -47,7 +47,7 @@ var _controllers_canvas = document.getElementById("fs_controllers"),
             init: function (o) {
                 var i = 0;
                 
-                o.values = new Array(16);
+                o.values = new Array(o.n);
                 
                 for (i = 0; i < o.values.length; i += 1) {
                     o.values[i] = 0;
@@ -57,6 +57,41 @@ var _controllers_canvas = document.getElementById("fs_controllers"),
                         type: "float",
                         count: o.values.length
                     };
+                
+                o.title = "float[" + o.values.length + "] " + o.name;
+                
+                _useProgram(_program);
+                _setUniforms(_gl, o.uniform.type, _program, o.name, o.values, o.uniform.comps);
+            },
+            position: null
+        },
+        xypad: {
+            draw: null,
+            opts: {
+                n: 2,
+                values: null,
+                onChange: function (o) {
+                    var uniform_location = _getUniformLocation(o.name, _program);
+                    
+                    _useProgram(_program);
+                    _gl.uniform2fv(uniform_location, new Float32Array(o.values));
+                }
+            },
+            init: function (o) {
+                var i = 0;
+                
+                o.values = new Array(o.n);
+                
+                for (i = 0; i < o.values.length; i += 1) {
+                    o.values[i] = 0.5;
+                }
+                
+                o["uniform"] = {
+                        type: "vec",
+                        comps: 2
+                    };
+                
+                o.title = "vec" + o.uniform.comps + " " + o.name;
                 
                 _useProgram(_program);
                 _setUniforms(_gl, o.uniform.type, _program, o.name, o.values, o.uniform.comps);
@@ -515,6 +550,111 @@ var _buildControls = function (ctrl_obj) {
     }
 };
 
+var _addHitHash = function (c, i, eo) {
+    var color;
+    
+    if (i >= c.hit_hashes.length) {
+        color = _decimalToHTMLColor(_hit_curr_color);
+
+        c.hit_hashes[i] = "#" + color;
+
+        _controllers_hit_hashes[color] = eo;
+
+        _hit_curr_color += 1;
+    }
+};
+
+var _eventXYPad = function (x, y, e) {
+    var c = e.c,
+        n = e.n,
+        
+        area_width = _controllers_settings.width - 16,
+        area_height = _controllers_settings.height - 16,
+        
+        vx, vy;
+    
+    if (x < (e.c.position.x + 8)) {
+        vx = 0;
+    } else if (x > (c.position.x + (_controllers_settings.width - 8))) {
+        vx = 1;
+    } else {
+        vx = (x - 8 - c.position.x) / area_width;
+    }
+    
+    if (y < (e.c.position.y + 8)) {
+        vy = 0;
+    } else if (y > (c.position.y + (_controllers_settings.height - 8))) {
+        vy = 1;
+    } else {
+        vy = (y - 8 - c.position.y) / area_height;
+    }
+    
+    c.values[n] = vx;
+    c.values[n + 1] = vy;
+    
+    _drawXYPad(_controllers_canvas_ctx, c, c.position.x, c.position.y, n);
+    
+    c.onChange(c);
+};
+
+var _drawXYPad = function (ctx, c, x, y, n) {
+    var i = 0,
+        
+        palette = _controllers_settings.palette,
+        
+        controller_width = _controllers_settings.width,
+        controller_height = _controllers_settings.height,
+        
+        area_width = _controllers_settings.width - 16,
+        area_height = _controllers_settings.height - 16,
+        
+        hit_ctx = _hit_canvas_ctx,
+        
+        ly = 0,
+        
+        lh = 6,
+        
+        ii = 0,
+        
+        vx = 0,
+        vy = 0,
+        
+        i_start = 0,
+        i_end = c.n,
+        
+        color = "";
+    
+    ctx.fillStyle = "#111111";
+    ctx.fillRect(x, y, controller_width, controller_height);
+    
+    hit_ctx.fillStyle = "#000000";
+    hit_ctx.fillRect(x, y, controller_width, controller_height);
+    
+    ctx.beginPath();
+    hit_ctx.beginPath();
+    for (i = i_start; i < i_end; i += 2) {
+        ii = i / 2;
+        
+        ctx.fillStyle = palette[ii % palette.length];
+        
+        vx = c.values[i];
+        vy = c.values[i + 1];
+        
+        //ctx.arc(x + 8 + Math.round(area_width * vx), y + 8 + Math.round(area_height * vy), 8, 0, 2 * Math.PI);
+        //ctx.fill();
+        ctx.fillRect(x + Math.round(area_width * vx), y + Math.round(area_height * vy), 16, 16);
+        
+        _addHitHash(c, ii, { f: _eventXYPad, e: { c: c, n: i } })
+
+        hit_ctx.fillStyle = c.hit_hashes[ii];
+        //hit_ctx.arc(x + 8 + Math.round(area_width * vx), y + 8 + Math.round(area_height * vy), 8, 0, 2 * Math.PI);
+        //hit_ctx.fill();
+        hit_ctx.fillRect(x + Math.round(area_width * vx), y + Math.round(area_height * vy), 16, 16);
+    }
+    hit_ctx.closePath();
+    ctx.closePath();
+};
+
 var _eventMultislider = function (x, y, e) {
     var c = e.c,
         n = e.n,
@@ -584,15 +724,7 @@ var _drawMultislider = function (ctx, c, x, y, n) {
         ctx.fillRect(x + w * i, ly + lh, w, Math.max((controller_height - lh) - (iv * (controller_height - lh)), 0));
         
         // hit testing
-        if (i >= c.hit_hashes.length) {
-            color = _decimalToHTMLColor(_hit_curr_color);
-            
-            c.hit_hashes[i] = "#" + color;
-            
-            _controllers_hit_hashes[color] = { f: _eventMultislider, e: { c: c, n: i } };
-            
-            _hit_curr_color += 1;
-        }
+        _addHitHash(c, i, { f: _eventMultislider, e: { c: c, n: i } })
         
         hit_ctx.fillStyle = c.hit_hashes[i];
         hit_ctx.fillRect(x + w * i, y, w, controller_height);
@@ -642,17 +774,17 @@ var _drawControls = function () {
             continue;
         }
         
-        x = margin + lcw + (c.id % lc) * controller_width_m;
-        y = margin + hcw + Math.floor(c.id / lc) * controller_height_m;
+        x = Math.round(margin + lcw + (c.id % lc) * controller_width_m);
+        y = Math.round(margin + hcw + Math.floor(c.id / lc) * controller_height_m);
 
         ctx.fillStyle = '#111111';
         ctx.fillRect(x, y, controller_width, controller_height);
-        
+
         _hit_canvas_ctx.fillStyle = '#000000';
         _hit_canvas_ctx.fillRect(x, y, controller_width, controller_height);
         
         ctx.fillStyle = 'white';
-        ctx.fillText(c.name, x + controller_width / 2, y - 2);
+        ctx.fillText(c.title, x + controller_width / 2, y - 2);
         
         _controllers[c.type].draw(ctx, c, x, y);
         
@@ -688,6 +820,7 @@ var _addControlWidget = function (type) {
                 id: _controls.length,
                 type: type,
                 name: "control" + _controls.length,
+                title: "",
                 hit_hashes: []
             },
             
@@ -714,14 +847,17 @@ var _addControlWidget = function (type) {
 
 var _setControllersCanvasDim = function () {
     _controllers_canvas.style.position = "relative";
-    _controllers_canvas.width = window.innerWidth / 1.5;
-    _controllers_canvas.height = window.innerHeight / 1.5;
+    _controllers_canvas.width = Math.round(window.innerWidth / 1.5);
+    _controllers_canvas.height = Math.round(window.innerHeight / 1.5);
 
     _hit_canvas.width = _controllers_canvas.width;
     _hit_canvas.height = _controllers_canvas.height;
     
-    _controllers_canvas_ctx.translate(0.5, 0.5);
-    _hit_canvas_ctx.translate(0.5, 0.5);
+    _controllers_canvas_ctx.imageSmoothingEnabled = false;
+    _hit_canvas_ctx.imageSmoothingEnabled = false;
+    
+    //_controllers_canvas_ctx.translate(0.5, 0.5);
+    //_hit_canvas_ctx.translate(0.5, 0.5);
 };
 
 /***********************************************************
@@ -736,6 +872,16 @@ WUI_ToolBar.create("fs_controls_toolbar", {
         {
             controls: [
                 {
+                    text: "OSC",
+                    on_click: _addControlWidget("osc"),
+                    tooltip: "Add OSC controller"
+                },
+                {
+                    text: "XY Pad",
+                    on_click: _addControlWidget("xypad"),
+                    tooltip: "Add XY pad"
+                },
+                {
                     text: "Multislider",
                     on_click: _addControlWidget("multislider"),
                     tooltip: "Add a multislider"
@@ -744,6 +890,7 @@ WUI_ToolBar.create("fs_controls_toolbar", {
         });
 
 _controllers.multislider.draw = _drawMultislider;
+_controllers.xypad.draw = _drawXYPad;
 
 _controllers_canvas.addEventListener("mousemove", function (e) {
     e.preventDefault();
@@ -763,10 +910,10 @@ _controllers_canvas.addEventListener("mousemove", function (e) {
     _hit_y = hit_y;
     
     if (_controllers_hit_hashes.hasOwnProperty(hit_color)) {
-        _hit_under_cursor = _controllers_hit_hashes[hit_color];
-        
         if (_hit_curr) {
             _hit_curr.f(_hit_x, _hit_y, _hit_under_cursor.e);
+        } else {
+            _hit_under_cursor = _controllers_hit_hashes[hit_color];
         }
         
         if (document.body.style.cursor !== "pointer") {
